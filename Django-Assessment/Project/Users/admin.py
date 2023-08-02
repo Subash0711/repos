3cb222ff9f857a312b43b75152d80d9d42a4593d
unknown <subasy820@gmail.com>
2023-08-02 13:00:42 +0530

    Remove Django assessment

--- Django-Assessment/Project/Users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Users.models import Users
-
-# Register your models here.
-admin.site.register(Users)
--- Django-Assessment/Project/Users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('registration/',views.userRegistration_view,name='User Resgistration'),
-    path('login/',views.userLogin_view,name='User Login')
-]
