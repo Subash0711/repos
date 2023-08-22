7fdaeee56f5a43c4c0d1b0041b1f25680e6c640a
unknown <subasy820@gmail.com>
2023-08-22 20:03:41 +0530

    Added the SQL files in the project

---
+++ main/SQL_files/Core_insert_details.sql
@@ -0,0 +1,76 @@
+INSERT INTO city (name) VALUES
+('New York'),
+('Los Angeles'),
+('Miami');
+
+INSERT INTO educationlevel (education_level) VALUES
+('High School'),
+('Bachelor Degree'),
+('Master Degree'),
+('Ph.D.');
+
+INSERT INTO educationqualification (education_qualification) VALUES
+('Computer Science'),
+('Engineering'),
+('Business Administration');
+
+INSERT INTO educationspecialization (education_specialization) VALUES
+('Software Development'),
+('Mechanical Engineering');
+
+
+INSERT INTO employeedirectory (referred_by) VALUES
+('Alice Johnson'),
+('Robert Smith');
+
+INSERT INTO eventdetails (event) VALUES
+('Tech Conference'),
+('Job Fair');
+
+INSERT INTO experiencelevel (experience_level) VALUES
+('Entry Level'),
+('Senior Level'),
+('Executive Level');
+
+INSERT INTO gender (gender) VALUES
+('Male'),
+('Female'),
+('Other');
+
+INSERT INTO jobrequisition (job_position) VALUES
+('Software Engineer'),
+('Marketing Manager'),
+('Sales Representative');
+
+INSERT INTO maritalstatus (marital_status) VALUES
+('Single'),
+('Married'),
+('Divorced'),
+('Widowed');
+
+
+INSERT INTO persona (persona) VALUES
+('Technical Professional'),
+('Creative Enthusiast');
+
+INSERT INTO reasonforchange (reason_for_change) VALUES
+('Better Growth Opportunity'),
+('Company Culture'),
+('Relocation');
+
+INSERT INTO screeningmode (screening_mode) VALUES
+('In-Person Interview'),
+('Online Assessment');
+
+INSERT INTO source (name) VALUES
+('XYZ University'),
+('LinkedIn'),
+('Referral'),
+('Career Fair');
+
+INSERT INTO sourcetype (type_name) VALUES
+('Online Platform'),
+('Employee Referral'),
+('Job Fair'),
+('Networking Event');
+
